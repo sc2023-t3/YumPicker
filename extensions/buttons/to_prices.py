82af04c27e3fa8c2627e8f39e1875d9aa5bd61c6
Nat1an <nathan@nat1an.xyz>
2023-07-22 10:45:51 +0800

    📦 chore(to_prices.py): add new file to implement the "to_prices" functionality ✨ feat(to_prices.py): implement the "ask_prices" function to handle the callback query and display a keyboard with price options ✨ feat(to_prices.py): add setup function to register the "ask_prices" function as a callback query handler in the application

---
+++ extensions/buttons/to_prices.py
@@ -0,0 +1,20 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes, Application, CallbackQueryHandler
+
+
+async def ask_prices(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+
+    await query.answer()
+
+    keyboard = [
+        [InlineKeyboardButton("$", callback_data="to_types")],
+        [InlineKeyboardButton("$$", callback_data="to_types")],
+        [InlineKeyboardButton("$$$", callback_data="to_types")]
+    ]
+
+    await query.edit_message_text(text="你錢包有多深？", reply_markup=InlineKeyboardMarkup(keyboard))
+
+
+def setup(application: Application):
+    application.add_handler(CallbackQueryHandler(ask_prices, pattern="to_prices"))
